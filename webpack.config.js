7e1875a3e91bcfccd66f3322dbc56bd983668918
Atos Luis Leal <109114725+0Mando@users.noreply.github.com>
2022-08-04 12:29:15 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode : 'development',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devtool : 'source-map',
+    devServer : {
+        static : {
+            directory : path.resolve(__dirname, 'dist')
+        },
+        port : 3000,
+        open : true,
+        hot : true,
+        compress : true,
+        historyApiFallback : true,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
